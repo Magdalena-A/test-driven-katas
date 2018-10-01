a6846a7427056e9dfc8229d4ebc48c98e98396dd
Magdalena Aichinger <magdalena.aichinger@gmail.com>
2018-10-01 11:59:42 -0400

    debugging mode - testing specs against provided solution

--- wrap.js
+++ wrap.js
@@ -1,10 +1,51 @@
-const wrap = (line, maxLen) => {
-  if(maxLen > line.length) {
+
+function wrap(line, maxLen) { 
+  if (line.length <= maxLen) { 
     return line;
-  } else {
-    const lineBreak = " ";
-    return line.replace(lineBreak, "/n")
   }
-};
+
+  const indexOfBlank = line.lastIndexOf(' ', maxLen);
+  let split;
+  let offset;
+  if (indexOfBlank > -1) {
+    split = indexOfBlank;
+    offset = 1;
+  } else { 
+    split = maxLen;
+    offset = 0;
+  }
+
+  return line.substring(0, split) + '\n' + wrap(line.substring(split + offset), maxLen)
+}
+// const wrap = (line, maxLen) => {
+//   let returnStr = '';
+
+//   if (maxLen > line.length) {
+//     return line;
+//   } else {
+//     const words = line.split(' '); // ['happy', 'monday']
+
+//     let i = 0;
+//     let newLine = '';
+
+//     while (i < words.length) { 
+
+//       if ((newLine += words[i]).length > maxLen) {
+//         newLine += '\n';
+//         returnStr += newLine;
+//         newLine = '';
+//       } else {
+//         newLine += words[i] + ' ';
+//         i++;
+//       }
+
+//     }
+      
+//     console.log(returnStr);
+//     return returnStr;
+//   }
+// };
+
+// console.log('test', wrap('Happy Monday', 7))
 
 module.exports = wrap;
--- wrap.spec.js
+++ wrap.spec.js
@@ -11,6 +11,14 @@ describe('wrap', () => {
   })
 
   it('Breaks the string before the column length is exceeded', () => {
+    console.log('test test test Happy Monday, 7',(wrap('Happy Monday', 7)))
     expect(wrap('Happy Monday', 7).indexOf('/n')).to.equal(5)
   })
+
+  it('Returns a string where each line length is less the column length', () => {
+    const brokenString = wrap("Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.", 20);
+    const brokenStringArr = brokenString.split('/n');
+    expect(brokenStringArr.every((line) => line.length <= 20)).to.equal(true);
+  })
+
 })
